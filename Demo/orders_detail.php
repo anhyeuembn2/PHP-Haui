19b624ac7dce24db640d1ee29692dfc60b0f0ddf
trieu2002 <trieunguyenbaby13@gmail.com>
2023-06-14 09:04:22 +0700

    commit:upload 1

--- Demo/orders_detail.php
+++ Demo/orders_detail.php
@@ -5,24 +5,24 @@ $items = array();
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    $sql = "SELECT * FROM orders_detail p INNER JOIN tblitems q ON p.itemId=q.id WHERE p.id=$id";
-    $item = $connect->query($sql);
-    $arr=mysqli_fetch_array($item);
+    $sql = "SELECT * FROM orders_detail p INNER JOIN tblitems q ON p.itemId=q.id WHERE p.orders_id=$id";
+    $kq = $connect->query($sql);
+    $arr = mysqli_fetch_array($kq);
     
-    
-    
-
-    
-}
-?>
-
+    if ($arr) {
+        ?>
 <div>
-
-
-    <h1>Mã đơn hàng : <?= $arr['id'] ?></h1>
-    <h2>Tên sản phẩm : <?= $arr['name'] ?></h2>
-    <p>Số lượng : <?= $arr[2] ?></p>
-    <p>Giá sản phảm : <?= $arr['price'] ?></p>
-
-
+    <h1>Mã đơn hàng: <?= $arr['id'] ?></h1>
+    <h2>Tên sản phẩm: <?= $arr['name'] ?></h2>
+    <p>Số lượng: <?= $arr[2] ?></p>
+    <p>Giá sản phẩm: <?= $arr['price'] ?></p>
 </div>
+<?php
+    } else {
+        echo "Không tìm thấy đơn hàng có ID: $id";
+    }
+}
+
+
+
+?>
